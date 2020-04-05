81400df63c3c2e5987cb9f6c9a9af0cacdd9f1aa
Dmitriy A <dmitriy@iinet.net.au>
2020-04-05 21:23:10 +1000

    add coonection for production

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://Dmitriy:Password123@ds223509.mlab.com:23509/heroku_58186572", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
